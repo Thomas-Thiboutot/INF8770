8bcad0023ba6919aeb7d99ceaa6ed14b2bb3071c
Thomas-Thiboutot <90419892+Thomas-Thiboutot@users.noreply.github.com>
2024-01-10 11:45:50 -0500

    test image init

--- TP1/LZW_encoding.py
+++ TP1/LZW_encoding.py
@@ -1,33 +1,32 @@
 ## Inspiré du code du cours 
 import time
 import numpy as np
+from PIL import Image
+
 ## TODO get message from data 
-def compress_text_LZW(filenumber: str):
-    print("texte numéro: " + filenumber)
-    Message = ""
-    f = open("./data/textes/texte_"+ filenumber +".txt")
-    for line in f.readlines():
-        Message += line
+def compress_text_LZW(filenumber: str, is_text: bool, message: str):
+
+    Message = "" if is_text else message
+    
+    if is_text:
+        f = open("./data/textes/texte_"+ filenumber +".txt")
+        for line in f.readlines():
+            Message += line
+            
     dictsymb =[Message[0]]
     dictbin = ["{:b}".format(0)]
     nbsymboles = 1
-
-
     for i in range(1,len(Message)):
         if Message[i] not in dictsymb:
             dictsymb += [Message[i]]
             dictbin += ["{:b}".format(nbsymboles)] 
             nbsymboles +=1
-
     longueurOriginale = np.ceil(np.log2(nbsymboles))*len(Message)
-
     for i in range(nbsymboles):
         dictbin[i] = "{:b}".format(i).zfill(int(np.ceil(np.log2(nbsymboles))))
-
     dictsymb.sort()
     dictionnaire = np.transpose([dictsymb,dictbin])
-    ## print(dictionnaire) 
-
+    print(dictionnaire) 
     i=0;
     MessageCode = []
     longueur = 0
@@ -35,14 +34,12 @@ def compress_text_LZW(filenumber: str):
     while i < len(Message):
         precsouschaine = Message[i] #sous-chaine qui sera codé
         souschaine = Message[i] #sous-chaine qui sera codé + 1 caractère (pour le dictionnaire)
-
         #Cherche la plus grande sous-chaine. On ajoute un caractère au fur et à mesure.
         while souschaine in dictsymb and i < len(Message):
             i += 1
             precsouschaine = souschaine
             if i < len(Message):  #Si on a pas atteint la fin du message
                 souschaine += Message[i]  
-
         #Codage de la plus grande sous-chaine à l'aide du dictionnaire  
         codebinaire = [dictbin[dictsymb.index(precsouschaine)]]
         MessageCode += codebinaire
@@ -52,24 +49,31 @@ def compress_text_LZW(filenumber: str):
             dictsymb += [souschaine]
             dictbin += ["{:b}".format(nbsymboles)] 
             nbsymboles +=1
-
         #Ajout de 1 bit si requis
         if np.ceil(np.log2(nbsymboles)) > len(MessageCode[-1]):
             for j in range(nbsymboles):
                 dictbin[j] = "{:b}".format(j).zfill(int(np.ceil(np.log2(nbsymboles))))
-
     ## print(MessageCode)
     end = time.perf_counter()
     dictionnaire = np.transpose([dictsymb,dictbin])
-    ## print(dictionnaire) 
+    print(dictionnaire) 
     tauxcompression = 1 - longueur/longueurOriginale
     print("Taux de compression :" + str(tauxcompression))
     print("Temps de codage: "+ str(end-start)+'\n')
-    ## print("Longueur = {0}".format(longueur))
-    ## print("Longueur originale = {0}".format(longueurOriginale))
-    
+    print("Longueur = {0}".format(longueur))
+    print("Longueur originale = {0}".format(longueurOriginale))
 
+## References: https://www.geeksforgeeks.org/how-to-manipulate-the-pixel-values-of-an-image-using-python/
+def compress_img_LZW(filenumber: str):
+    print("image numéro: " + filenumber)
+    with open("./data/images/image_"+ filenumber +".png", "rb") as image2string: 
+        converted_string = image2string.read() 
+        print(converted_string) 
+    compress_text_LZW(filenumber, False, converted_string)
+    
 if __name__ == "__main__":
-    for i in range(1,6):
-        compress_text_LZW(str(i))
+    ##for i in range(1,6):
+    ##    compress_text_LZW(str(i),True, "")
+    ##for i in range(1,6):
+    compress_img_LZW(str(1))
         
