320ec8f970631fe974d72fc834c60b5e165daff3
hv <huy-viet.nguyen@polymtl.ca>
2024-03-27 11:34:30 -0400

    traitement vidéo

--- TP3/moodle/src/nn_index.py
+++ TP3/moodle/src/nn_index.py
@@ -1,6 +1,7 @@
 # Code modifié de Mehdi Miah
 
 import os
+import time
 
 import torch
 import torchvision
@@ -9,9 +10,7 @@ import torchvision.models as models
 
 from PIL import Image
 from einops import rearrange
-from IPython.display import display
-import matplotlib.pyplot as plt
-
+import cv2
 
 PATH = '../data/'
 
@@ -22,10 +21,10 @@ def process_image():
     return image
 
 
-def calculate_descriptor(model, image):
+def calculate_img_descriptor(model, image):
     # Pré-processing
     preprocess = transforms.Compose([
-        transforms.Resize((224, 224)),                       # change la taille de l'image en 224x224
+        transforms.Resize((224, 224)),                      # change la taille de l'image en 224x224
         transforms.ToTensor(),                              # convertit une image PIL ou numpy.ndarray (HxWxC) dans la plage [0, 255] en un torch.FloatTensor de forme (CxHxW) dans la plage [0.0, 1.0]
         transforms.Normalize(mean=[0.485, 0.456, 0.406],    # normalise les valeurs 
                              std=[0.229, 0.224, 0.225]),
@@ -46,6 +45,29 @@ def calculate_descriptor(model, image):
 
     return output
 
+def calculate_video_descriptors(model, path):
+    video = cv2.VideoCapture(path)
+    descriptors = []
+    while video.isOpened():
+        ret, frame = video.read()
+        if not ret:
+            break
+        descriptors.append(calculate_img_descriptor(model, Image.fromarray(frame)))
+
+    return descriptors
+    
+
+def write_descriptors_to_csv(model):
+    with open('nn_descriptors.csv', 'w') as f:
+        f.write('filename, descriptor\n')
+
+        for i, filename in enumerate(os.listdir(PATH + 'jpeg')):
+            image = Image.open(PATH + 'jpeg/' + filename)
+            descriptor = calculate_img_descriptor(model, image)
+            f.write('{file}, {desc}\n'.format(file=filename, desc=descriptor.cpu().numpy()))
+
+    return
+
 
 if __name__ == '__main__':
     ### Utiliser GPU si disponible, sinon CPU
@@ -57,11 +79,11 @@ if __name__ == '__main__':
     model.eval()
     model.to(device)
 
-    with open('nn_descriptors.csv', 'w') as f:
-        f.write('filename, descriptor\n')
+    if not os.path.exists('nn_descriptors.csv'):
+        write_descriptors_to_csv(model)
 
-        for i, filename in enumerate(os.listdir(PATH + 'jpeg')):
-            image = Image.open(PATH + 'jpeg/' + filename)
-            descriptor = calculate_descriptor(model, image)
-            f.write('{file}, {desc}\n'.format(file=filename, desc=descriptor.cpu().numpy()))
-        
+    start = time.perf_counter()
+    calculate_video_descriptors(model, PATH + 'mp4/v001.mp4')
+    end = time.perf_counter()
+
+    print(f'Processing time: {end - start} s')
