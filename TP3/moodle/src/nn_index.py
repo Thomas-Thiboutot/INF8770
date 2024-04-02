a64edd1178e9de2f5d5e5aa5cbb11655a92f7a13
hv <huy-viet.nguyen@polymtl.ca>
2024-04-02 14:48:28 -0400

    recherche

--- TP3/moodle/src/nn_index.py
+++ TP3/moodle/src/nn_index.py
@@ -9,11 +9,13 @@ import torchvision.models as models
 from PIL import Image
 from einops import rearrange
 import cv2
-from numpy import savetxt, loadtxt
+from numpy import savetxt, loadtxt, dot
+from numpy.linalg import norm
 import argparse
 
 PATH = '../data/'
 COMPRESS_RATIO = 1
+FPS = 30  # Trames/s 
 
 
 # Fonction issue du code modifié de Mehdi Miah
@@ -60,27 +62,36 @@ def calculate_video_descriptors(model, path):
     return descriptors
     
 
-def write_descriptors_to_txt(model):
-    descriptors = []
+def generate_image_descriptors(model):
+    image_descriptors = []
+    image_names = []
 
     for filename in os.listdir(PATH + 'jpeg'):
         print("Calcul du descripteur pour l'image:", filename, end='\r')
         image = Image.open(PATH + 'jpeg/' + filename)
         descriptor = calculate_img_descriptor(model, image)
-        descriptors.append(descriptor.cpu().numpy())
+        image_descriptors.append(descriptor.cpu().numpy())
+        image_names.append(filename.replace('.jpeg', ''))
     print()
-    
-    savetxt(PATH + 'nn/nn_descriptors.txt', descriptors)
-            
-    return
+
+    return image_descriptors, image_names
 
 
-def write_video_descriptors_to_txt(model):
-    for filename in os.listdir(PATH + 'mp4'):
+def generate_video_descriptors(model):
+    video_descriptors = []
+    files = os.listdir(PATH + 'mp4')
+
+    for filename in files:
         print("Calcul des descripteurs pour la vidéo:", filename, end='\r')
         descriptors = calculate_video_descriptors(model, PATH + 'mp4/' + filename)
-        savetxt(PATH + 'nn/videos/' + filename + '.txt', descriptors)
+        video_descriptors.append((filename.replace('.mp4', ''), descriptors))
     print()
+
+    return video_descriptors
+
+def write_video_descriptors_to_txt(video_descriptors: list[tuple[str, list]]):
+    for video_descriptor in video_descriptors:
+        savetxt(PATH + 'nn/videos/' + video_descriptor[0] + '.txt', video_descriptor[1])
 
     return
 
@@ -89,43 +100,82 @@ def load_video_descriptors(folder_path):
     video_descriptors = []
 
     for filename in os.listdir(folder_path):
-        video_descriptors.append(loadtxt(folder_path + '/' + filename))
+        video_descriptors.append((filename, loadtxt(folder_path + '/' + filename)))
 
     return video_descriptors
 
 
-def create_index(re_index: bool):
+def create_index(re_index: bool, no_save: bool):
+    image_descriptors = []
+    video_descriptors = []
+    image_names = []
+
+    ### Chargement des descripteurs d'image
+    if not os.path.exists(PATH + 'nn'):
+        os.makedirs(PATH + 'nn')
+
     if not os.path.exists(PATH + 'nn/nn_descriptors.txt') or re_index:
-        print("Écriture des descripteurs du réseau de neuronnes dans nn_descriptors.txt...")
+        print("Calcul des descripteurs d'images")
         start = time.perf_counter()
-        write_descriptors_to_txt(model)
+        image_descriptors, image_names = generate_image_descriptors(model)
         end = time.perf_counter()
-        print("Écriture complétée des descripteurs d'image en", end - start, 'secondes.')
+        print("Calcul des descripteurs d'images terminé en", end - start, "secondes")
+        
+        if no_save:
+            print("Écriture des descripteurs du réseau de neuronnes dans nn_descriptors.txt...")
+            savetxt(PATH + 'nn/nn_descriptors.txt', image_descriptors)
+            print("Écriture complétée.")
 
     else:
-        print("Les descripteurs d'images du réseau de neuronnes existent déjà.")
+        print("Les descripteurs d'images du réseau de neuronnes existent déjà. Chargement des descripteurs.")
+        image_descriptors = loadtxt(PATH + 'nn/nn_descriptors.txt')
+        image_names = [s.replace('.jpeg', '') for s in os.listdir(PATH + 'jpeg')]
 
-    image_descriptors = loadtxt(PATH + 'nn/nn_descriptors.txt')
     assert len(os.listdir(PATH + 'jpeg')) == len(image_descriptors)
 
+    ### Chargement des descripteurs de vidéo
+    if not os.path.exists(PATH + 'nn/videos'):
+        os.makedirs(PATH + 'nn/videos')
+
     if not len(os.listdir(PATH + 'mp4')) == len(os.listdir(PATH + 'nn/videos')) or re_index:
-        print("Écritures des descripteurs des vidéos...")
+        print("Calcul des descripteurs des vidéos:")
         start = time.perf_counter()
-        write_video_descriptors_to_txt(model)
+        video_descriptors = generate_video_descriptors(model)
         end = time.perf_counter()
-        print("Écriture complétée des descripteurs des vidéos en", end - start, 'secondes')
+        print("Calcul des descripteurs des vidéos terminé en", end - start, "secondes.")
+
+        if no_save:
+            print("Écritures des descripteurs des vidéos...")
+            write_video_descriptors_to_txt(video_descriptors)
+            print("Écriture complétée.")
     else:
         print("Les descripteurs de vidéos du réseau de neuronnes existent déjà.")
-
-    video_descriptors = load_video_descriptors(PATH + 'nn/videos')
+        video_descriptors = load_video_descriptors(PATH + 'nn/videos')
+    
     assert len(os.listdir(PATH + 'mp4')) == len(video_descriptors)
 
-    return image_descriptors, video_descriptors
+    return image_names, image_descriptors, video_descriptors
+
+
+def cosine_sim(vector1, vector2) -> float:
+    return dot(vector1, vector2) / (norm(vector1) * norm(vector2))
+
+
+def find_video(image_descriptor, video_descriptors):
+    for video_name, descriptors in video_descriptors:
+        for i, descriptor in enumerate(descriptors):
+            similarity = cosine_sim(descriptor, image_descriptor)
+
+            if similarity >= 0.87:
+                return (video_name, i * COMPRESS_RATIO / FPS )
+                
+    return ('out', '')
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-i', action='store_true', help="Effectue l'indexation peut importe s'il y a déjà des descripteurs")
+    parser.add_argument('-n', '--nosave', action='store_false', help="Désactive la sauvegarde des descripteurs")
 
     args = parser.parse_args()
 
@@ -141,11 +191,23 @@ if __name__ == '__main__':
     ### Phase d'indexation: chargement des descripteurs
     print("Phase d'indexation: chargement des descripteurs...")
     start = time.perf_counter()
-    image_descriptors, video_descriptors = create_index(args.i)
+    image_names, image_descriptors, video_descriptors = create_index(args.i, args.nosave)
     end = time.perf_counter()
     print("Phase d'indexation terminée en:", end - start, 'secondes')
 
     ### Phase de recherche
+    print("Début de la phase de recherche")
     start = time.perf_counter()
-    
+    with open('nn_answer.csv', 'w') as answers:
+        for idx, image_descriptor in enumerate(image_descriptors):
+            print("Image:", image_names[idx], end='\r')
+            result = find_video(image_descriptor, video_descriptors)
+            answers.write('{i},{v},{t}\n'.format(i=image_names[idx], v=result[0], t=result[1]))
+            if (idx == 3):
+                break
+        print()
     end = time.perf_counter()
+
+    search_time = end - start
+    print("Recherche terminée en", search_time, "secondes.")
+    print("Temps moyen par image:", search_time / len(image_names))
